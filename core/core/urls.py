f3bbed9caf7c941d3cdf1242cef8bb47492733f8
DevPrinceK <princesamuelpks@gmail.com>
2023-07-22 14:23:40 +0000

    update

---
+++ core/core/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("main.urls")),
+]
+
+
+# Let django serve static files in development mode
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
