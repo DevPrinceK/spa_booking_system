f0f8f04c4f9b1f05de6b0028bb16d5ff669970d6
DevPrinceK <princesamuelpks@gmail.com>
2023-07-22 14:38:52 +0000

    update

--- core/main/views.py
+++ core/main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class BaseView(View):
+    template = "main/base.html"
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template, {})
